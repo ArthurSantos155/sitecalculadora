835bf0e0b7a5c57b296fcf1200aae4dd08cc3056
ArthurSantos155 <atua0837@gmail.com>
2026-03-27 17:30:10 -0300

    ADA
    Da

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function so(){
+    if (!validar()) return;
 let n1 = parseFloat(document.getElementById('n1').value);
 let n2 = parseFloat(document.getElementById('n2').value);
 
@@ -8,6 +9,7 @@ document.getElementById('resultado').textContent = resultado;
 }
 
 function su(){
+    if (!validar()) return;
 let n1 = parseFloat(document.getElementById('n1').value);
 let n2 = parseFloat(document.getElementById('n2').value);
 
@@ -17,6 +19,7 @@ document.getElementById('resultado').textContent = resultado;
 }
 
 function mu(){
+    if (!validar()) return;
 let n1 = parseFloat(document.getElementById('n1').value);
 let n2 = parseFloat(document.getElementById('n2').value);
 
@@ -26,6 +29,7 @@ document.getElementById('resultado').textContent = resultado;
 }
 
 function di(){
+    if (!validar()) return;
 let n1 = parseFloat(document.getElementById('n1').value);
 let n2 = parseFloat(document.getElementById('n2').value);
 
@@ -42,6 +46,7 @@ else{
 }
 }
 function pow(){
+    if (!validar()) return;
 let n1 = parseFloat(document.getElementById('n1').value);
 let n2 = parseFloat(document.getElementById('n2').value);
 
@@ -51,6 +56,7 @@ document.getElementById('resultado').textContent = resultado;
 }
 
 function raiz(){
+    if (!validar()) return;
 let n1 = parseFloat(document.getElementById('n1').value);
 let n2 = parseFloat(document.getElementById('n2').value);
 
@@ -60,6 +66,7 @@ document.getElementById('resultado').textContent = resultado;
 }
 
 function fato(){
+    if (!validar()) return;
 let n1 = parseFloat(document.getElementById('n1').value);
 let n2 = parseFloat(document.getElementById('n2').value);
 
@@ -73,10 +80,30 @@ document.getElementById('resultado').textContent = resultado;
 }
 
 function porc(){
+    if (!validar()) return;
 let n1 = parseFloat(document.getElementById('n1').value);
 let n2 = parseFloat(document.getElementById('n2').value);
 
 let resultado = (n1 / n2) * 100 ;
 
 document.getElementById('resultado').textContent = resultado;
+}
+
+function validar(){
+    
+    let n1 = parseFloat(document.getElementById('n1').value);
+    let n2 = parseFloat(document.getElementById('n2').value);
+
+    if (isNaN(n1) || isNaN(n2)){
+        document.getElementById('resultado').textContent = "oi";
+        return false;
+    }
+    return true;
+}
+
+function limp(){
+    document.getElementById('n1').value = "";
+    document.getElementById('n2').value = "";
+    document.getElementById('resultado').textContent = "";
+
 }
